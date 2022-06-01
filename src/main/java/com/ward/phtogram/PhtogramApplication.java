e3ec27c2b1297529c0f74574aff5c5f8b73b30fd
ward <bonuswon@gmail.com>
2022-06-01 23:42:28 +0900

    Create photogram-intellij-gradle

---
+++ src/main/java/com/ward/phtogram/PhtogramApplication.java
@@ -0,0 +1,13 @@
+package com.ward.phtogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhtogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhtogramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ward/phtogram/PhtogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ward.phtogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhtogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
